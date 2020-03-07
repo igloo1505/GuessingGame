4dbed4204f15ce7984b48895872ff4ff9ce7e305
Andrew Mueller <iglooworks1505@gmail.com>
2020-03-06 18:28:10 -0600

    finally gave up on the array idea and am going with constantly rerendering the string

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
-var main = document.querySelectorAll("#mainTitle path");
-console.log("connected");
-console.log(main.length);
+var questions = [
+  {
+    question:
+      "This Person said 'There is nothing more to be learned in physics' two years before Einstein wrote Special Relativity?",
+    answer: "Kelvin"
+  },
+  {
+    question: "Einstein's miracle year is known as?",
+    answer: "Annus mirabilis"
+  }
+];
 
-for (var i = 0; i < main.length; i++) {
-  console.log(`letter ${i + 1} has a length of ${main[i].getTotalLength()}`);
+console.log(questions.length);
+let Random = {};
+const setQuestion = () => {
+  Random = questions[Math.floor(Math.random() * questions.length)];
+};
+setQuestion();
+const { question, answer } = Random;
+
+var questionDocument = document.getElementById("question");
+var answerDocument = document.getElementById("answer");
+
+document.onkeydown = () => {
+  console.log(event.key);
+};
+
+console.log(answer.length);
+
+questionDocument.innerHTML = question;
+
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+let word = "";
+for (var i = 0; i < answer.length; i++) {
+  if (alphabet.includes(answer[i])) {
+    console.log("_");
+    word = word += "_";
+  } else if (answer[i] === " ") {
+    word = word += " ";
+  }
 }
+answerDocument.innerHTML = word;
---
+++ questions.js
@@ -0,0 +1,9 @@
+ [{
+         "question": "This Person said 'There is nothing more to be learned in physics' two years before Einstein wrote Special Relativity?",
+         "answer": "Kelvin"
+     },
+     {
+         "question": "Einstein's miracle year is known as?",
+         "answer": "Annus mirabilis"
+     }
+ ]
