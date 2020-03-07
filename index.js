b695d3bb3012708200d77145fb8d8aabf8b5f132
Andrew Mueller <iglooworks1505@gmail.com>
2020-03-06 23:58:33 -0600

    working up to loading new letters

--- index.js
+++ index.js
@@ -2,70 +2,115 @@ var questions = [
   {
     question:
       "This Person said 'There is nothing more to be learned in physics' two years before Einstein wrote Special Relativity?",
-    answer: "Kelvin"
+    answer: "kelvin"
   },
   {
     question: "Einstein's miracle year is known as?",
-    answer: "Annus mirabilis"
+    answer: "annus mirabilis"
   }
 ];
 
-console.log(questions.length);
-let Random = {};
-const setQuestion = () => {
-  Random = questions[Math.floor(Math.random() * questions.length)];
-};
-setQuestion();
-const { question, answer } = Random;
+const setPlay = () => {
+  console.log(questions.length);
+  let Random = {};
+  const setQuestion = () => {
+    Random = questions[Math.floor(Math.random() * questions.length)];
+  };
+  setQuestion();
+  const { question, answer } = Random;
 
-var questionDocument = document.getElementById("question");
-var answerDocument = document.getElementById("answer");
+  var questionDocument = document.getElementById("question");
+  var answerDocument = document.getElementById("answer");
+  var turnsDocument = document.getElementById("turns");
+  var guessesDocument = document.getElementById("guesses");
 
-document.onkeydown = () => {
-  //   alert("fix alignment of index page and commit");
-  console.log(event.key);
-};
+  console.log(answer.length);
 
-console.log(answer.length);
+  questionDocument.innerHTML = question;
 
-questionDocument.innerHTML = question;
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  const setPlay = () => {
+    guessed = [];
+    guessedCorrectly = [];
+    guessesRemaining = 10;
+  };
 
-let alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
-
-let word = "";
-for (var i = 0; i < answer.length; i++) {
-  if (alphabet.includes(answer[i])) {
-    console.log("_");
-    word = word += "_";
-  } else if (answer[i] === " ") {
-    word = word += " ";
+  let word = "";
+  for (var i = 0; i < answer.length; i++) {
+    if (alphabet.includes(answer[i])) {
+      console.log("_");
+      word = word += "_";
+    } else if (answer[i] === " ") {
+      word = word += " ";
+    }
   }
-}
-answerDocument.innerHTML = word;
+  answerDocument.innerHTML = word;
+
+  let guessed = [];
+  let guessedCorrectly = [];
+  let guessesRemaining = 10;
+
+  document.onkeydown = () => {
+    let key = event.key;
+    // console.log("answer index" + answer.indexOf(key));
+    if (alphabet.includes(key)) {
+      if (answer.indexOf(key) === -1 && guessed.indexOf(key) === -1) {
+        console.log("guessed" + guessed);
+        guessed.push(key);
+        guessesRemaining--;
+        // console.log(guessesRemaining);
+      } else if (
+        answer.indexOf(key) !== -1 &&
+        guessedCorrectly.indexOf(key) == -1
+      ) {
+        guessedCorrectly.push(key);
+        let allIndexes = [];
+        for (var i = 0; i < answer.length; i++) {
+          if (answer[i] === key) {
+            console.log("index is " + i);
+            console.log("letter is " + answer[i]);
+            word = word.replace(word[i], key);
+            console.log(word);
+            allIndexes.push(answer[i]);
+          }
+        }
+
+        if (guessedCorrectly.length == answer.length) {
+          alert("YAY! You're like Einstein");
+          setPlay();
+        }
+      }
+
+      turnsDocument.innerHTML = guessesRemaining;
+      guessesDocument.innerHTML = guessed.join("");
+    }
+  };
+};
+setPlay();
